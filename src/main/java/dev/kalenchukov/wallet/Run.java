fa2dc117dd977ab72d1402f7c4904e5afd16456d
Алексей Каленчуков <aleksey.kalenchukov@yandex.ru>
2023-10-06 18:37:26 +0300

    Инициализация проекта

---
+++ src/main/java/dev/kalenchukov/wallet/Run.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2023 Алексей Каленчуков
+ * GitHub: https://github.com/kalenchukov
+ * E-mail: mailto:aleksey.kalenchukov@yandex.ru
+ */
+
+package dev.kalenchukov.wallet;
+
+/**
+ * Класс запуска приложения.
+ */
+public class Run
+{
+	/**
+	 * Метод запуска.
+	 *
+	 * @param args параметры.
+	 */
+	public static void main(String[] args)
+	{
+
+	}
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+/**
+ * Определяет API по управлению счетами игроков.
+ */
+module dev.kalenchukov.wallet
+{
+	exports dev.kalenchukov.wallet;
+}
